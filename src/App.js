28f2eee81009c81c70a28eeb49935c987b1ebf0d
Dean Kang <dean@nexybit.com>
2019-11-06 17:49:19 +0900

    lint 설정

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './style.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  render() {
+    return (
+      <div className="container">
+        <h3 className="title">Image Site</h3>
+        <div className="area_input">
+          <input type="text" placeholder="검색어를 입력하세요" />
+          <button type="submit">검색</button>
+        </div>
+        <div className="area_img"></div>
+      </div>
+    )
+  }
 }
-
 export default App;
