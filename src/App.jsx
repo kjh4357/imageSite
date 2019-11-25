fa7ddc92a4ed3e8246e2c96d31c88c0557ae2133
Dean Kang <dean@nexybit.com>
2019-11-25 17:00:18 +0900

    로딩시 랜덤이미지 api 적용 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ImmutablePropTypes from 'react-immutable-proptypes';
 import { connect } from 'react-redux';
 import './style.scss';
 import { handleBackgroundImageDataLoadAction } from '@util/store/modules/home';
 
 class App extends Component {
+  static propTypes = {
+    homeStore: ImmutablePropTypes.contains({
+      backgroundImageData: PropTypes.object,
+      isBackgroundImageFetchedSuccess: PropTypes.bool,
+    }).isRequired,
+    handleBackgroundImageDataLoad: PropTypes.func.isRequired,
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageUrl: '',
+    };
+  }
+  static getDerivedStateFromProps(nextProps) {
+    const {
+      backgroundImageData,
+      isBackgroundImageFetchedSuccess,
+    } = nextProps.homeStore.toJS();
+    if (isBackgroundImageFetchedSuccess) {
+      return { imageUrl: backgroundImageData.urls.small };
+    }
+
+    return null;
+  }
   componentDidMount() {
-    handleBackgroundImageDataLoadAction();
+    this.props.handleBackgroundImageDataLoad();
   }
   render() {
+    const { imageUrl } = this.state;
+    console.log(imageUrl);
     return (
       <div className="container">
         <h3 className="title">Image Site</h3>
@@ -17,7 +45,9 @@ class App extends Component {
           <input type="text" placeholder="검색어를 입력하세요" />
           <button type="submit">검색</button>
         </div>
-        <div className="area_img"></div>
+        <div className="area_img">
+          <img src={imageUrl} />
+        </div>
       </div>
     );
   }
--- util/api/image.js
+++ util/api/image.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
-export const fetchRandomImage = params =>
+export const fetchRandomImage = () =>
   axios
-    .get(`https://source.unsplash.com/daily`)
+    .get('https://api.unsplash.com/photos/random/', {
+      headers: { 
+        Authorization: 'Client-ID b9084ee78ffee616f0275925d4eb139fc214b6c33fa03b41f0bd8f190eaa302d'
+      } 
+    })
     .then(resp => resp)
     .catch(err => err);
 
